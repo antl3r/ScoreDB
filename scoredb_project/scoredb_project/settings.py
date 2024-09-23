dbcb3cb62ba023ebcbd597bbfc7eff5a569b5b3f
fuckoff.sergei <killyourself@example.com>
2024-09-23 10:48:13 +0700

    DB settings

--- scoredb_project/scoredb_project/settings.py
+++ scoredb_project/scoredb_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'scoredb_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students_db',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost',  # Or the address of your MySQL server
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
